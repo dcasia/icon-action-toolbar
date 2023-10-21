7e2e37abb3b92e2b0b7532ed3c3f95ab041b2384
Rafael Milewski <rafael.milewski@gmail.com>
2023-10-21 18:36:32 +0800

    Add missing default actions (Replicate / Preview / Impersonate) (#4)

--- resources/js/components/ActionDropdown.vue
+++ resources/js/components/ActionDropdown.vue
@@ -26,7 +26,7 @@
             :data="actionResponseData"
         />
 
-        <IconActionToolbar :actions="actions" @click="handleActionClick" :standalone="true"/>
+        <IconActionToolbar :parent-type="parentType" :actions="availableActions" @click="onClick" :standalone="true"/>
 
     </div>
 
@@ -35,9 +35,11 @@
 <script setup>
 
     import { useActions } from '@/composables/useActions'
+    import { useLocalization } from '@/composables/useLocalization'
     import IconActionToolbar from './IconActionToolbar.vue'
+    import { computed, getCurrentInstance } from 'vue'
 
-    const emitter = defineEmits([ 'actionExecuted' ])
+    const emitter = defineEmits([ 'actionExecuted', 'show-preview' ])
 
     const props = defineProps({
         resourceName: {},
@@ -65,7 +67,27 @@
         actionResponseData,
     } = useActions(props, emitter, Nova.store)
 
+    const { __ } = useLocalization()
+    const instance = getCurrentInstance()
+
     const runAction = () => executeAction(() => emitter('actionExecuted'))
+    const parentType = instance.parent.vnode.type.__file
+
+    const onClick = event => {
+
+        const action = availableActions.value.find(element => element.uriKey === event)
+
+        if (typeof action.onClick === 'function') {
+
+            action.onClick()
+
+        } else {
+
+            handleActionClick(event)
+
+        }
+
+    }
 
     const handleResponseModalConfirm = () => {
         closeResponseModal()
@@ -76,5 +98,77 @@
         closeResponseModal()
         emitter('actionExecuted')
     }
+
+    const availableActions = computed(() => {
+
+        const actions = [ ...props.actions ]
+        const resource = instance.parent?.props?.resource
+        const currentUser = Nova.store.getters[ 'currentUser' ]
+        const config = Nova.config('icon_action_toolbar')
+
+        if (resource) {
+
+            if (resource.authorizedToReplicate) {
+
+                actions.push({
+                    name: __('Replicate'),
+                    uriKey: '__replicate-action__',
+                    iconActionToolbar: { icon: config.icons.replicate },
+                    onClick: () => {
+
+                        const url = instance.ctx.$url(`/resources/${ props.resourceName }/${ resource.id.value }/replicate`, {
+                            viaResource: props.viaResource,
+                            viaResourceId: props.viaResourceId,
+                            viaRelationship: props.viaRelationship,
+                        })
+
+                        Nova.visit(url)
+
+                    },
+                })
+
+            }
+
+            if (resource.authorizedToView && resource.previewHasFields) {
+
+                actions.push({
+                    name: __('Preview'),
+                    uriKey: '__preview-action__',
+                    iconActionToolbar: { icon: config.icons.preview },
+                    onClick: () => instance.parent.emit('show-preview'),
+                })
+
+            }
+
+            if (currentUser.canImpersonate && resource.authorizedToImpersonate) {
+
+                actions.push({
+                    name: __('Impersonate'),
+                    uriKey: '__impersonate-action__',
+                    iconActionToolbar: { icon: config.icons.impersonate },
+                    onClick: () => instance.parent.ctx.startImpersonating({
+                        resource: props.resourceName,
+                        resourceId: resource.id.value,
+                    }),
+                })
+
+            }
+
+            if (resource.authorizedToDelete && !resource.softDeleted) {
+
+                actions.push({
+                    name: __('Delete Resource'),
+                    uriKey: '__delete-resource-action__',
+                    iconActionToolbar: { icon: config.icons.delete_resource },
+                    onClick: () => instance.parent.ctx.openDeleteModal(),
+                })
+
+            }
+
+        }
+
+        return actions
+
+    })
 
 </script>
