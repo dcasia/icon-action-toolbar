60a11592ed0c669abbc7ae7f88978b4aa42a4d64
Bartłomiej Gajda <b.gajda@webard.me>
2024-09-25 23:33:33 +0200

    wip

--- resources/js/components/ActionDropdown.vue
+++ resources/js/components/ActionDropdown.vue
@@ -3,172 +3,162 @@
     <div class="icon-action-toolbar">
 
         <!-- Confirm Action Modal -->
-        <component
-            v-if="actionModalVisible"
-            :show="actionModalVisible"
-            class="text-left"
-            :is="selectedAction?.component"
-            :working="working"
-            :selected-resources="selectedResources"
-            :resource-name="resourceName"
-            :action="selectedAction"
-            :errors="errors"
-            @confirm="runAction"
-            @close="closeConfirmationModal"
-        />
+        <component v-if="actionModalVisible" :show="actionModalVisible" class="text-left"
+            :is="selectedAction?.component" :working="working" :selected-resources="selectedResources"
+            :resource-name="resourceName" :action="selectedAction" :errors="errors" @confirm="runAction"
+            @close="closeConfirmationModal" />
 
-        <component
-            v-if="responseModalVisible"
-            :show="responseModalVisible"
-            :is="actionResponseData?.modal"
-            @confirm="handleResponseModalConfirm"
-            @close="handleResponseModalClose"
-            :data="actionResponseData"
-        />
-
-        <IconActionToolbar :parent-type="parentType" :actions="availableActions" @click="onClick" :standalone="true"/>
-
+        <component v-if="responseModalVisible" :show="responseModalVisible" :is="actionResponseData?.modal"
+            @confirm="handleResponseModalConfirm" @close="handleResponseModalClose" :data="actionResponseData" />
     </div>
+
+    <IconActionToolbar :parent-type="parentType" :actions="availableActions" @click="onClick" :standalone="true" />
+
+
 
 </template>
 
 <script setup>
 
-    import { useActions } from '@/composables/useActions'
-    import { useLocalization } from '@/composables/useLocalization'
-    import IconActionToolbar from './IconActionToolbar.vue'
-    import { computed, getCurrentInstance } from 'vue'
+import { useActions } from '@/composables/useActions'
+import { useLocalization } from '@/composables/useLocalization'
+import IconActionToolbar from './IconActionToolbar.vue'
+import { computed, getCurrentInstance } from 'vue'
 
-    const emitter = defineEmits([ 'actionExecuted', 'show-preview' ])
+const emitter = defineEmits(['actionExecuted', 'show-preview'])
 
-    const props = defineProps({
-        resourceName: {},
-        viaResource: {},
-        viaResourceId: {},
-        viaRelationship: {},
-        relationshipType: {},
-        actions: { type: Array, default: [] },
-        selectedResources: { type: [ Array, String ], default: () => [] },
-        endpoint: { type: String, default: null },
-        triggerDuskAttribute: { type: String, default: null },
-    })
+const props = defineProps({
+    resourceName: {},
+    viaResource: {},
+    viaResourceId: {},
+    viaRelationship: {},
+    relationshipType: {},
+    actions: { type: Array, default: [] },
+    selectedResources: { type: [Array, String], default: () => [] },
+    endpoint: { type: String, default: null },
+    triggerDuskAttribute: { type: String, default: null },
+})
 
-    const {
-        errors,
-        actionModalVisible,
-        responseModalVisible,
-        openConfirmationModal,
-        closeConfirmationModal,
-        closeResponseModal,
-        handleActionClick,
-        selectedAction,
-        working,
-        executeAction,
-        actionResponseData,
-    } = useActions(props, emitter, Nova.store)
+const {
+    errors,
+    actionModalVisible,
+    responseModalVisible,
+    openConfirmationModal,
+    closeConfirmationModal,
+    closeResponseModal,
+    handleActionClick,
+    selectedAction,
+    working,
+    executeAction,
+    actionResponseData,
+} = useActions(props, emitter, Nova.store)
 
-    const { __ } = useLocalization()
-    const instance = getCurrentInstance()
+const { __ } = useLocalization()
+const instance = getCurrentInstance()
 
-    const runAction = () => executeAction(() => emitter('actionExecuted'))
-    const parentType = instance.parent.vnode.type.__file
+const runAction = () => executeAction(() => emitter('actionExecuted'))
+const parentType = instance.parent.vnode.type.__file
 
-    const onClick = event => {
+const onClick = event => {
 
-        const action = availableActions.value.find(element => element.uriKey === event)
+    const action = availableActions.value.find(element => element.uriKey === event)
 
-        if (typeof action.onClick === 'function') {
+    if (typeof action.onClick === 'function') {
 
-            action.onClick()
+        action.onClick()
 
-        } else {
+    } else {
 
-            handleActionClick(event)
+        handleActionClick(event)
+
+    }
+
+}
+
+const handleResponseModalConfirm = () => {
+    closeResponseModal()
+    emitter('actionExecuted')
+}
+
+const handleResponseModalClose = () => {
+    closeResponseModal()
+    emitter('actionExecuted')
+}
+
+const availableActions = computed(() => {
+
+    const actions = [...props.actions]
+    const resource = instance.parent?.props?.resource
+    const currentUser = Nova.store.getters['currentUser']
+    const config = Nova.config('icon_action_toolbar')
+
+    if (resource) {
+
+        if (resource.authorizedToReplicate) {
+
+            actions.push({
+                name: __('Replicate'),
+                uriKey: '__replicate-action__',
+                iconActionToolbar: { icon: config.icons.replicate },
+                onClick: () => {
+
+
+
+                    const url = instance.ctx.$url(`/resources/${props.resourceName}/${resource.id.value}/replicate`, {
+                        viaResource: props.viaResource,
+                        viaResourceId: props.viaResourceId,
+                        viaRelationship: props.viaRelationship,
+                    }).replace(Nova.config('base'), '')
+
+                    console.log(url);
+
+                    Nova.visit(url)
+
+                },
+            })
+
+        }
+
+        if (resource.authorizedToView && resource.previewHasFields) {
+
+            actions.push({
+                name: __('Preview'),
+                uriKey: '__preview-action__',
+                iconActionToolbar: { icon: config.icons.preview },
+                onClick: () => instance.parent.emit('show-preview'),
+            })
+
+        }
+
+        if (currentUser.canImpersonate && resource.authorizedToImpersonate) {
+
+            actions.push({
+                name: __('Impersonate'),
+                uriKey: '__impersonate-action__',
+                iconActionToolbar: { icon: config.icons.impersonate },
+                onClick: () => instance.parent.ctx.startImpersonating({
+                    resource: props.resourceName,
+                    resourceId: resource.id.value,
+                }),
+            })
+
+        }
+
+        if (resource.authorizedToDelete && !resource.softDeleted && Nova.$router.page.component !== 'Nova.Index') {
+
+            actions.push({
+                name: __('Delete Resource'),
+                uriKey: '__delete-resource-action__',
+                iconActionToolbar: { icon: config.icons.delete_resource },
+                onClick: () => instance.parent.ctx.openDeleteModal(),
+            })
 
         }
 
     }
 
-    const handleResponseModalConfirm = () => {
-        closeResponseModal()
-        emitter('actionExecuted')
-    }
+    return actions
 
-    const handleResponseModalClose = () => {
-        closeResponseModal()
-        emitter('actionExecuted')
-    }
-
-    const availableActions = computed(() => {
-
-        const actions = [ ...props.actions ]
-        const resource = instance.parent?.props?.resource
-        const currentUser = Nova.store.getters[ 'currentUser' ]
-        const config = Nova.config('icon_action_toolbar')
-
-        if (resource) {
-
-            if (resource.authorizedToReplicate) {
-
-                actions.push({
-                    name: __('Replicate'),
-                    uriKey: '__replicate-action__',
-                    iconActionToolbar: { icon: config.icons.replicate },
-                    onClick: () => {
-
-                        const url = instance.ctx.$url(`/resources/${ props.resourceName }/${ resource.id.value }/replicate`, {
-                            viaResource: props.viaResource,
-                            viaResourceId: props.viaResourceId,
-                            viaRelationship: props.viaRelationship,
-                        })
-
-                        Nova.visit(url)
-
-                    },
-                })
-
-            }
-
-            if (resource.authorizedToView && resource.previewHasFields) {
-
-                actions.push({
-                    name: __('Preview'),
-                    uriKey: '__preview-action__',
-                    iconActionToolbar: { icon: config.icons.preview },
-                    onClick: () => instance.parent.emit('show-preview'),
-                })
-
-            }
-
-            if (currentUser.canImpersonate && resource.authorizedToImpersonate) {
-
-                actions.push({
-                    name: __('Impersonate'),
-                    uriKey: '__impersonate-action__',
-                    iconActionToolbar: { icon: config.icons.impersonate },
-                    onClick: () => instance.parent.ctx.startImpersonating({
-                        resource: props.resourceName,
-                        resourceId: resource.id.value,
-                    }),
-                })
-
-            }
-
-            if (resource.authorizedToDelete && !resource.softDeleted && Nova.$router.page.component !== 'Nova.Index') {
-
-                actions.push({
-                    name: __('Delete Resource'),
-                    uriKey: '__delete-resource-action__',
-                    iconActionToolbar: { icon: config.icons.delete_resource },
-                    onClick: () => instance.parent.ctx.openDeleteModal(),
-                })
-
-            }
-
-        }
-
-        return actions
-
-    })
+})
 
 </script>
--- resources/js/components/IconActionToolbar.vue
+++ resources/js/components/IconActionToolbar.vue
@@ -1,79 +1,79 @@
 <template>
+    <div :class="{ 'icon-action-toolbar-wrapper': standalone && isDetailView }">
+        <div v-if="actions.length > 0"
+            :class="{ 'rounded': standalone, 'rounded-lg bg-gray-700/5 dark:bg-gray-700': !standalone }"
+            class="flex dark:focus:ring-gray-600 justify-evenly">
 
-    <div v-if="actions.length > 0"
-         :class="{ 'rounded': standalone, 'rounded-lg bg-gray-700/5 dark:bg-gray-700': !standalone }"
-         class="flex dark:focus:ring-gray-600 justify-evenly">
+            <div v-for="{ iconActionToolbar, destructive, uriKey, name } of actions">
+                <button v-tooltip="name" v-if="iconActionToolbar" type="button"
+                    @click.stop="() => $emit('click', uriKey)" :class="{
+                        'dark:hover:[&:not(:disabled)]:text-primary-500': parentType === 'ActionSelector',
+                        'w-auto': !isDetailView && !standalone,
+                        'px-3 w-auto': !isDetailView && parentType === 'LoadingView.vue',
+                        'hover:[&:not(:disabled)]:text-red-400 dark:hover:[&:not(:disabled)]:text-red-400 w-9': destructive && !isDetailView,
+                        'hover:[&:not(:disabled)]:text-primary-500 dark:hover:[&:not(:disabled)]:text-primary-500 w-9': !destructive && !isDetailView,
+                        'rounded hover:[&:not(:disabled)]:text-primary-200 dark:hover:[&:not(:disabled)]:text-primary-800 focus:outline-none focus:ring px-3 hover:text-gray-500': standalone && isDetailView
+                    }"
+                    class="inline-flex items-center justify-center toolbar-button h-9 dark:text-gray-400 text-gray-500 disabled:opacity-50 disabled:pointer-events-none">
 
-        <div v-for="{ iconActionToolbar, destructive, uriKey, name } of actions">
+                    <Icon v-if="typeof iconActionToolbar.icon !== 'string'" name="cube-transparent" />
 
-            <button
-                v-tooltip="name"
-                v-if="iconActionToolbar"
-                type="button"
-                @click.stop="() => $emit('click', uriKey)"
-                :class="{
-                    'dark:hover:text-primary-500': parentType === 'ActionSelector',
-                    'w-auto': !isDetailView && !standalone,
-                    'hover:text-red-500': destructive && !isDetailView,
-                    'dark:hover:text-primary-500 hover:text-primary-500': !destructive && !isDetailView,
-                    'rounded hover:bg-gray-200 dark:hover:bg-gray-800 focus:outline-none focus:ring px-3 hover:text-gray-500': standalone && isDetailView
-                }"
-                class="toolbar-button h-9 dark:text-gray-400 text-gray-500 px-2 disabled:opacity-50 disabled:pointer-events-none">
+                    <template v-else-if="iconActionToolbar.icon?.trim()?.startsWith('<svg')">
+                        <div v-html="iconActionToolbar.icon" />
+                    </template>
 
-                <Icon v-if="typeof iconActionToolbar.icon !== 'string'" name="cube-transparent"/>
+                    <Icon v-else-if="iconActionToolbar.icon" :name="iconActionToolbar.icon" />
 
-                <template v-else-if="iconActionToolbar.icon?.trim()?.startsWith('<svg')">
-                    <div v-html="iconActionToolbar.icon"/>
-                </template>
+                    <div class="ml-1 whitespace-nowrap" v-if="iconActionToolbar.label">
+                        {{ iconActionToolbar.label }}
+                    </div>
 
-                <Icon v-else-if="iconActionToolbar.icon" :name="iconActionToolbar.icon"/>
+                </button>
 
-                <div class="ml-1 whitespace-nowrap" v-if="iconActionToolbar.label">
-                    {{ iconActionToolbar.label }}
-                </div>
-
-            </button>
+            </div>
 
         </div>
-
     </div>
 
 </template>
 
 <script>
 
-    import { Icon, Button } from 'laravel-nova-ui'
+import { Icon, Button } from 'laravel-nova-ui'
 
-    export default {
-        components: { Icon, Button },
-        emits: [ 'click' ],
-        props: [ 'actions', 'standalone', 'parentType' ],
-        computed: {
-            isDetailView() {
-                return Nova.$router.page.component === 'Nova.Detail'
-                    && this.parentType === 'DetailActionDropdown.vue'
-            },
+export default {
+    components: { Icon, Button },
+    emits: ['click'],
+    props: ['actions', 'standalone', 'parentType'],
+    computed: {
+        isDetailView() {
+            return Nova.$router.page.component === 'Nova.Detail'
+                && this.parentType === 'DetailActionDropdown.vue'
         },
-    }
+    },
+}
 
 </script>
 
 <style lang="scss">
+.icon-action-toolbar-wrapper {
+    width: calc(100% - 48px);
+    overflow: scroll;
+}
 
-    div[dusk$="detail-component"] div.icon-action-toolbar {
-        @apply mx-0;
+div[dusk$="detail-component"] div.icon-action-toolbar {
+    @apply mx-0;
+}
+
+table div.icon-action-toolbar .hover-element {
+
+    &:hover {
+        @apply bg-transparent #{!important};
     }
 
-    table div.icon-action-toolbar .hover-element {
-
-        &:hover {
-            @apply bg-transparent #{!important};
-        }
-
-        button > div {
-            @apply px-3;
-        }
-
+    button>div {
+        @apply px-3;
     }
 
+}
 </style>
